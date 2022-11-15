85d95d3782e34b25064bfb3d3c7054adffa4fbf6
JaquiSue24 <99093475+JaquiSue24@users.noreply.github.com>
2022-11-15 10:08:13 -0800

    added config | One time launch
    Onboarding Screen launch one time
    Used Shared Preference

--- app/src/main/java/com/paszlelab/dcroarapp/OnboardingPage.java
+++ app/src/main/java/com/paszlelab/dcroarapp/OnboardingPage.java
@@ -2,11 +2,28 @@ package com.paszlelab.dcroarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class OnboardingPage extends AppCompatActivity {
+
+    String prevStarted = "yes";
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedpreferences = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        if (!sharedpreferences.getBoolean(prevStarted, false)) {
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putBoolean(prevStarted, Boolean.TRUE);
+            editor.apply();
+        } else {
+            goToLogin();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +31,7 @@ public class OnboardingPage extends AppCompatActivity {
         setContentView(R.layout.activity_onboarding);
     }
 
-    public void goToLogin(View v){
+    public void goToLogin(){
         Intent intent = new Intent(this, InitialPage.class);
         startActivity(intent);
     }
