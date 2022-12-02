0aecff3fa4fa0dbb6b4d81fd8813ba3e2f3cd73d
Tyrone Pastoril <56924437+kentnexus@users.noreply.github.com>
2022-12-02 02:27:38 -0800

    2/12/2022 2:27AM

---
+++ app/src/main/java/com/paszlelab/dcroarapp/Adapters/FindUsersAdapter.java
@@ -0,0 +1,64 @@
+package com.paszlelab.dcroarapp.Adapters;
+
+import android.graphics.Bitmap;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.paszlelab.dcroarapp.databinding.FriendItemLayoutBinding;
+import com.paszlelab.dcroarapp.models.Student;
+
+import java.util.List;
+
+public class FindUsersAdapter extends RecyclerView.Adapter<FindUsersAdapter.UserViewHolder>{
+
+    private final List<Student> students;
+
+    public FindUsersAdapter(List<Student> students){
+        this.students = students;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        FriendItemLayoutBinding friendItemLayoutBinding = FriendItemLayoutBinding.inflate(
+                LayoutInflater.from(parent.getContext()), parent, false);
+        return new UserViewHolder(friendItemLayoutBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        holder.setUserData(students.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return students.size();
+    }
+
+    class UserViewHolder extends RecyclerView.ViewHolder {
+
+        FriendItemLayoutBinding binding;
+
+        UserViewHolder(FriendItemLayoutBinding friendItemLayoutBinding){
+            super(friendItemLayoutBinding.getRoot());
+            binding = friendItemLayoutBinding;
+        }
+
+        void setUserData(Student student){
+            binding.txtFriendName.setText(student.getFirstName()+" "+student.getLastName());
+            binding.txtEmailAddress.setText(student.getEmailAddress());
+//            binding.imgUserProfile.setImageBitmap(getUserImage());
+        }
+    }
+
+    private Bitmap getUserImage(String encodedImage){
+        byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes,0, bytes.length);
+    }
+}
