fb29b5fea45651685d1bea4fb8aece562c589709
Tyrone Pastoril <56924437+kentnexus@users.noreply.github.com>
2022-12-03 15:09:00 -0800

    3/12/2022 03:08PM
    fixed course listener
    TODO: course fragment UI

--- app/src/main/java/com/paszlelab/dcroarapp/Fragments/CoursesFragment.java
+++ app/src/main/java/com/paszlelab/dcroarapp/Fragments/CoursesFragment.java
@@ -35,14 +35,13 @@ public class CoursesFragment extends Fragment implements CourseListener {
     private FragmentCoursesBinding binding;
     private ArrayList<CourseModel> courseModelArrayList;
     CourseAdapter adapter;
-//    private CourseAdapter.CourseClickListener listener;
+    //    private CourseAdapter.CourseClickListener listener;
     private FirebaseFirestore db;
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        binding = FragmentCoursesBinding.inflate(inflater, container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentCoursesBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         db = FirebaseFirestore.getInstance();
 
@@ -52,137 +51,109 @@ public class CoursesFragment extends Fragment implements CourseListener {
         binding.searchId.setQueryHint("Type here");
         binding.searchId.setIconified(false);
         binding.searchId.clearFocus();
-//        binding.searchId.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            @Override
-//            public boolean onQueryTextSubmit(String query) {
-//                filteredSearch(query);
-//                return true;
-//            }
-//
-//            @Override
-//            public boolean onQueryTextChange(String newText) {
-//                filteredSearch(newText);
-//                return true;
-//            }
-//        });
+        binding.searchId.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchCourse(query);
+                return true;
+            }
 
-        getList();
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchCourse(newText);
+                return true;
+            }
+        });
+
 
         // Inflate the layout for this fragment
         return view;
     }
 
 
-
-    private void filteredSearch(String query) {
+    private void searchCourse(String query) {
         // creating a new array list to filter our data.
-        ArrayList<CourseModel> filteredlist = new ArrayList<CourseModel>();
-
-        for (CourseModel item : courseModelArrayList) {
-            // checking if the entered string matched with any item of our recycler view.
-            if (item.getCourseCode().toLowerCase().contains(query.toLowerCase())) {
-                // if the item is matched we are
-                // adding it to our filtered list.
-                Log.d("matched Course code", item.toString());
-                filteredlist.add(item);
-
-            }
-            else if (item.getCourseName().toLowerCase().contains(query.toLowerCase())) {
-                Log.d("matched Course name", item.toString());
-                filteredlist.add(item);
-            }
-        }
-
-        if(query.isEmpty()){
+        if (query.isEmpty()) {
             getCourses();
-            Toast.makeText(getContext(),"No Course Found..", Toast.LENGTH_SHORT).show();
-            Log.d("Empty filter: ", "no course found");
         } else {
-//            adapter = new CourseAdapter(filteredlist,listener, CoursesFragment.this);
-            binding.courseRView.setAdapter(adapter);
+            binding.courseRView.setAdapter(null);
+            filteredCourses(query);
         }
 
     }
 
     private void getCourses() {
-        setCourseOnClickListener();
-        courseModelArrayList = new ArrayList<CourseModel>();
+        db.collection("ChatRoom").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful() && !task.getResult().isEmpty()) {
 
-        // below line is to add data to our array list.
-//        courseModelArrayList.add(new CourseModel("BUSN 1198", "BUSINESS EXPLORATIONS I"));
-//        courseModelArrayList.add(new CourseModel("BUSN 1200", "FUNDAMENTALS OF BUSINESS"));
-//        courseModelArrayList.add(new CourseModel("CSIS 3175", "INTRODUCTION TO MOBILE APPLICATION DEVELOPMENT"));
-//        courseModelArrayList.add(new CourseModel("CSIS 1275", "INTRODUCTION TO PROGRAMMING II"));
-//        courseModelArrayList.add(new CourseModel("CSIS 1280", "MULTIMEDIA WEB DEVELOPMENT"));
+                List<CourseModel> courses = new ArrayList<>();
 
-//        fetching the courses from firebase firestore
+                for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                    String dept = queryDocumentSnapshot.getString("department");
+                    String courseCode = queryDocumentSnapshot.getString("courseCode");
+                    String name = queryDocumentSnapshot.getString("name");
 
+                    CourseModel course = new CourseModel();
+                    course.setCourseDept(dept);
+                    course.setCourseCode(courseCode);
+                    course.setCourseName(name);
+                    courses.add(course);
+                }
+                if (courses.size() > 0) {
+                    CourseAdapter courseAdapter = new CourseAdapter(courses, this);
+                    LinearLayoutManager manager = new LinearLayoutManager(getContext());
+                    binding.courseRView.setHasFixedSize(true);
 
-        // initializing our adapter class.
-//        adapter = new CourseAdapter(courseModelArrayList, listener, CoursesFragment.this );
+                    // setting layout manager
+                    // to our recycler view.
+                    binding.courseRView.setLayoutManager(manager);
 
-        // adding layout manager to our recycler view.
-        LinearLayoutManager manager = new LinearLayoutManager(getContext());
-        binding.courseRView.setHasFixedSize(true);
-
-        // setting layout manager
-        // to our recycler view.
-        binding.courseRView.setLayoutManager(manager);
-
-        // setting adapter to
-        // our recycler view.
-        binding.courseRView.setAdapter(adapter);
+                    // setting adapter to
+                    // our recycler view.
+                    binding.courseRView.setAdapter(courseAdapter);
+                } else {
+                    Toast.makeText(CoursesFragment.this.getContext(), "No courses found", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
-    private void setCourseOnClickListener() {
-//        listener = new CourseAdapter.CourseClickListener() {
-//            @Override
-//            public void onClick(View v, int position) {
-//                Intent intent = new Intent(getContext(), CoursePage.class);
-//                intent.putExtra("courseCode", courseModelArrayList.get(position).getCourseCode());
-//                intent.putExtra("courseName", courseModelArrayList.get(position).getCourseName());
-//                startActivity(intent);
-//            }
-//        };
-    }
+    private void filteredCourses(String query) {
+        db.collection("ChatRoom").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful() && !task.getResult().isEmpty()) {
 
-    private void getList(){
-        db.collection("ChatRoom")
-                .get()
-                .addOnCompleteListener(task->{
-                        if(task.isSuccessful() && !task.getResult().isEmpty()){
+                List<CourseModel> courses = new ArrayList<>();
 
-                            List<CourseModel> courses = new ArrayList<>();
+                for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                    String dept = queryDocumentSnapshot.getString("department");
+                    String courseCode = queryDocumentSnapshot.getString("courseCode");
+                    String name = queryDocumentSnapshot.getString("name");
 
-                            for(QueryDocumentSnapshot queryDocumentSnapshot:task.getResult()) {
-                                String dept = queryDocumentSnapshot.getString("department");
-                                String courseCode = queryDocumentSnapshot.getString("courseCode");
-                                String name = queryDocumentSnapshot.getString("name");
+                    if (dept.contains(query) || courseCode.contains(query) || name.contains(query)) {
+                        CourseModel course = new CourseModel();
+                        course.setCourseDept(dept);
+                        course.setCourseCode(courseCode);
+                        course.setCourseName(name);
+                        courses.add(course);
+                    }
+                }
+                if (courses.size() > 0) {
+                    CourseAdapter courseAdapter = new CourseAdapter(courses, this);
+                    LinearLayoutManager manager = new LinearLayoutManager(getContext());
+                    binding.courseRView.setHasFixedSize(true);
 
-                                CourseModel course = new CourseModel();
-                                course.setCourseDept(dept);
-                                course.setCourseCode(courseCode);
-                                course.setCourseName(name);
-                                courses.add(course);
-                            }
-                            if(courses.size() > 0){
-                                CourseAdapter courseAdapter = new CourseAdapter(courses, this);
-                                LinearLayoutManager manager = new LinearLayoutManager(getContext());
-                                binding.courseRView.setHasFixedSize(true);
+                    // setting layout manager
+                    // to our recycler view.
+                    binding.courseRView.setLayoutManager(manager);
 
-                                // setting layout manager
-                                // to our recycler view.
-                                binding.courseRView.setLayoutManager(manager);
-
-                                // setting adapter to
-                                // our recycler view.
-                                binding.courseRView.setAdapter(courseAdapter);
-                            }
-                        }
-                        else{
-                            Toast.makeText(CoursesFragment.this.getContext(), "No courses found",Toast.LENGTH_LONG).show();
-                        }
-                });
+                    // setting adapter to
+                    // our recycler view.
+                    binding.courseRView.setAdapter(courseAdapter);
+                } else {
+                    Toast.makeText(CoursesFragment.this.getContext(), "No courses found", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
